352e6bbc189c501fc0b28548e8a421ce5d835784
tappejaz <105188776+tappejaz@users.noreply.github.com>
2025-12-01 00:15:58 +0600

    spec code chnaged

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('Intentional failure test', async ({ page }) => {
+test('Intentional failure test', async ({ page, browser }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle('Example Domain'); // Wrong title on purpose
+  await browser.close();
+
 });
