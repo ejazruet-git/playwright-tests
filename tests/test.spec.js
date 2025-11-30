8e2975a7887a9a56a0129d317b4f1402ab9df215
tappejaz <105188776+tappejaz@users.noreply.github.com>
2025-12-01 00:01:07 +0600

    code changed

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Intentional failure test', async ({ page }) => {
   await page.goto('https://example.com');
-  await expect(page).toHaveTitle('ThisWillFail'); // Wrong title on purpose
+  await expect(page).toHaveTitle('Example Domain'); // Wrong title on purpose
 });
